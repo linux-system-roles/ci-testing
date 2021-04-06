8477986fc1214755d8a4c25ef09b05bbfd0d327d
Rich Megginson <rmeggins@redhat.com>
2021-04-06 10:05:28 -0600

    move some stuff to dockerfile

--- .github/actions/py26/entrypoint.sh
+++ .github/actions/py26/entrypoint.sh
@@ -4,19 +4,9 @@ set -euxo pipefail
 
 exec 2>&1
 
-for file in /etc/yum.repos.d/CentOS-*.repo; do
-  if ! grep '^baseurl=.*vault[.]centos[.]org' "$file"; then
-    sed -i -e 's,^mirrorlist,#mirrorlist,' \
-           -e 's,^#baseurl=,baseurl=,' \
-           -e 's,mirror.centos.org/centos/$releasever,vault.centos.org/6.10,' \
-           "$file"
-  fi
-done
-
-LSR_EXTRA_PACKAGES_YUM="${LSR_EXTRA_PACKAGES_YUM:-} libffi-devel openssl-devel dbus-devel python-devel"
-
-yum install -y epel-release git $LSR_EXTRA_PACKAGES_YUM
-yum install -y python-pip
+if [ -n "${LSR_EXTRA_PACKAGES_YUM:-}" ]; then
+  yum install -y $LSR_EXTRA_PACKAGES_YUM
+fi
 mkdir -p $HOME/.cache/pip/http
 # yum install python-pip brings in a very old version of setuptools - upgrade it first
 pip install --upgrade 'setuptools==28.8.*'
