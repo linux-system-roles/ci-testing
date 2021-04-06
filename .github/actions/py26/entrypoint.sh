c41a83175ad7bafb0fd3ae837c4b79f764f156bc
Rich Megginson <rmeggins@redhat.com>
2021-04-06 09:24:22 -0600

    try that

--- .github/actions/py26/entrypoint.sh
+++ .github/actions/py26/entrypoint.sh
@@ -17,11 +17,8 @@ LSR_EXTRA_PACKAGES_YUM="${LSR_EXTRA_PACKAGES_YUM:-} libffi-devel openssl-devel d
 
 yum install -y epel-release git $LSR_EXTRA_PACKAGES_YUM
 yum install -y python-pip
-echo home
-ls -alrtF $HOME
-#pip install --upgrade pip
-echo more
-ls -alrtF $HOME/.cache $HOME/.cache/pip || :
 mkdir -p $HOME/.cache/pip/http
-pip install 'tox<3' 'virtualenv==15.*' 'pluggy==0.5.*' 'py<1.5' "$TOX_LSR"
+# yum install python-pip brings in a very old version of setuptools - upgrade it first
+pip install --upgrade 'setuptools==28.8.*'
+pip install 'tox<3' 'virtualenv==15.*' 'pluggy==0.5.*' 'py<1.5' 'configparser<5' "$TOX_LSR"
 tox -e py26,coveralls,custom
