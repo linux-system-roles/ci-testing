372ff5bb253495c97236b467c6446f797d31cab5
Rich Megginson <rmeggins@redhat.com>
2021-04-05 19:01:41 -0600

    try with local action

---
+++ .github/actions/py26/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+set -euxo pipefail
+
+for file in /etc/yum.repos.d/CentOS-*.repo; do
+  if ! grep '^baseurl=.*vault[.]centos[.]org' "$file"; then
+    sed -i -e 's,^mirrorlist,#mirrorlist,' \
+           -e 's,^#baseurl=,baseurl=,' \
+           -e 's,mirror.centos.org/centos/$releasever,vault.centos.org/6.10,' \
+           "$file"
+  fi
+done
+
+yum install -y epel-release
+yum install -y python-pip
+pip install 'tox<3' 'virtualenv==15.*' 'pluggy==0.5.*' "$TOX_LSR"
+lsr_ci_preinstall
+tox -e py26,coveralls,custom
