d2dc749c7b0d1d6e070f079dde83e8f4064124df
Rich Megginson <rmeggins@redhat.com>
2021-04-05 19:37:09 -0600

    debug

--- .github/actions/py26/entrypoint.sh
+++ .github/actions/py26/entrypoint.sh
@@ -15,8 +15,10 @@ done
 
 yum install -y epel-release git
 yum install -y python-pip
+echo home
 ls -alrtF $HOME
 pip install --upgrade pip
+echo more
 ls -alrtF $HOME/.cache $HOME/.cache/pip || :
 ip install 'tox<3' 'virtualenv==15.*' 'pluggy==0.5.*' "$TOX_LSR"
 lsr_ci_preinstall
