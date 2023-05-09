3e6c6677562e5ee7c20a7769dec10913d4ae0fd6
Sergei Petrosian <30409084+spetrosi@users.noreply.github.com>
2023-05-09 12:24:42 +0200

    Revert "test: Don't write script from action instead put it into .github [citest skip]"
    This reverts commit 5ff1237b1abdde27c0f9b0a24ebc918d2c92fd32.

--- .pr_num_to_commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-commit_file=.commit_message.txt
-commit_title=$(git log --pretty=format:"%s" -1)
-git log --pretty=format:"%s #"$PR_NUMBER"%n%n%b" -1 > $commit_file
-git commit --amend --allow-empty -F $commit_file
-rm -f $commit_file
